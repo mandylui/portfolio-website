f643ca54e7c6ee7305b99773384b180d6f6d0b96
mandylui <mandyrlui@gmail.com>
2025-01-13 19:55:04 -0800

    about me page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,7 +16,16 @@ function About() {
           <div className="about-divider"></div>
         </div>
 
-        <div className="about-description">description here</div>
+        <div className="about-description">
+          Hi! I’m Mandy Lui, a junior at the University of Rochester pursuing a Bachelor's in Computer Science with a concentration in Human-Computer Interaction and a Business minor. I’m passionate about creating user-friendly designs and developing seamless front-end experiences that combine functionality with aesthetics.
+         <br></br><br></br>
+          I’ve worked on projects like Glimpse (a customizable album-making website), and Stay Positive (a mental health platform integrating affirmations, meditation, and journaling), which have allowed me to
+          solve real-world problems through research and thoughtful design & development processes.
+          <br></br><br></br>
+          Outside of coding, I enjoy spending time with family and friends, hiking, doing crafts, and playing guitar!
+          <br></br><br></br>
+          Feel free to reach out through any of my contacts provided on this website :)
+        </div>
         
     </main>
 
